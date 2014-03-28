74fb8d9604a2c640b6e0c204f45c8b2e8bad34dd
Oliver Charles <ollie@ocharles.org.uk>
2014-03-28 09:20:03 +0000

    Test literal numbers

--- test/Tests.hs
+++ test/Tests.hs
@@ -5,22 +5,25 @@ import Control.Applicative
 import Control.Monad.Identity (runIdentity)
 import Data.Aeson (decode)
 import Data.ByteString.Lazy.Char8 ()
+import Data.Ratio (denominator, numerator)
+import Data.Scientific (Scientific)
 import Data.Text (Text)
 import Test.HUnit ((@?=))
-import Text.Digestive (Form, (.:), check, text, listOf)
-import Text.Digestive.Aeson (digestJSON, jsonErrors)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (testCase)
+import Text.Digestive (Form, (.:), Result(..), check, text, listOf, stringRead, validate)
+import Text.Digestive.Aeson (digestJSON, jsonErrors)
 
 import qualified Data.Text as T
 
 --------------------------------------------------------------------------------
-data Pokemon = Pokemon { pokemonName :: Text }
+data Pokemon = Pokemon { pokemonName :: Text, pokemonNumber :: Int }
   deriving (Eq, Show)
 
 
 pokeForm :: Monad m => Form Text m Pokemon
 pokeForm = Pokemon <$> "name" .: nonEmptyText
+                   <*> "number" .: parseInteger
   where
     nonEmptyText = check "Name cannot be empty" (not . T.null) $
                      text Nothing
@@ -37,8 +40,8 @@ testPokemon = testGroup "Pokemon tests" [ testPokemonOk, testPokemonInvalid ]
     testPokemonOk = testCase "Submit pokeForm with valid data" $
         (runIdentity $ snd <$> digestJSON pokeForm json) @?= Just expected
       where
-        (Just json) = decode "{\"name\":\"Pikachu\"}"
-        expected = Pokemon { pokemonName = "Pikachu" }
+        (Just json) = decode "{\"name\":\"Pikachu\", \"number\":\"25\"}"
+        expected = Pokemon { pokemonName = "Pikachu", pokemonNumber = 25 }
 
     testPokemonInvalid =
         let (v, r) = runIdentity $ digestJSON pokeForm json
@@ -47,9 +50,9 @@ testPokemon = testGroup "Pokemon tests" [ testPokemonOk, testPokemonInvalid ]
              , testCase "jsonErrors shows correct errors" $ jsonErrors v @?= errors
              ]
       where
-        (Just json) = decode "{\"name\":\"\"}"
+        (Just json) = decode "{\"name\":\"\", \"number\":\"25\"}"
         (Just errors) = decode "{\"name\":\"Name cannot be empty\"}"
-        expected = Pokemon { pokemonName = "Pikachu" }
+        expected = Pokemon { pokemonName = "Pikachu", pokemonNumber = 25 }
 
 
 --------------------------------------------------------------------------------
@@ -67,16 +70,20 @@ testPokedex = testGroup "Pokedex tests"
     testPokedexSingle = testCase "Valid pokedex with one pokemon" $
         (runIdentity $ snd <$> digestJSON pokedexForm json) @?= Just expected
       where
-        (Just json) = decode "{\"pokemon\":[{\"name\": \"Pikachu\"}]}"
-        expected = Pokedex [Pokemon { pokemonName = "Pikachu" }]
+        (Just json) = decode "{\"pokemon\":[{\"name\": \"Pikachu\",\"number\":25}]}"
+        expected = Pokedex [Pokemon { pokemonName = "Pikachu", pokemonNumber = 25 }]
 
     testPokedexMany = testCase "Valid pokedex with many pokemon" $
         (runIdentity $ snd <$> digestJSON pokedexForm json) @?= Just expected
       where
-        (Just json) = decode "{\"pokemon\":[{\"name\": \"Pikachu\"}, {\"name\":\"Clefable\"}, {\"name\":\"Gengar\"}]}"
-        expected = Pokedex [ Pokemon { pokemonName = "Pikachu" }
-                           , Pokemon { pokemonName = "Clefable" }
-                           , Pokemon { pokemonName = "Gengar" }
+        (Just json) = decode "{\"pokemon\":[\
+                             \  {\"name\": \"Pikachu\", \"number\": 25},\
+                             \  {\"name\":\"Clefable\", \"number\": 36},\
+                             \  {\"name\":\"Gengar\", \"number\": 94}\
+                             \]}"
+        expected = Pokedex [ Pokemon { pokemonName = "Pikachu", pokemonNumber = 25 }
+                           , Pokemon { pokemonName = "Clefable", pokemonNumber = 36 }
+                           , Pokemon { pokemonName = "Gengar", pokemonNumber = 94 }
                            ]
 
     testPokedexFailHead =
@@ -115,3 +122,15 @@ main = defaultMain $ testGroup "Tests" [ testPokemon
                                        , testTopLevelLists
                                        ]
 
+--------------------------------------------------------------------------------
+validateInteger :: Num a => Scientific -> Result Text a
+validateInteger x =
+  let xRat = toRational x
+  in if denominator xRat /= 1
+       then Error "Number must be an integer"
+       else return (fromInteger $ numerator xRat)
+
+--------------------------------------------------------------------------------
+parseInteger :: (Monad m, Num a) => Form Text m a
+parseInteger =
+  validate validateInteger (stringRead "Could not parse number" Nothing)
