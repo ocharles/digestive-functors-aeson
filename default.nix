b66af9d4fe29b11dcb12587cf4814f25949e8b90
Oliver Charles <ollie@ocharles.org.uk>
2015-06-24 15:12:48 +0100

    Rewrite default.nix

--- default.nix
+++ default.nix
@@ -1,14 +1,20 @@
-{}:
-with import <nixpkgs> {};
-let
-  inherit (haskellPackages) aeson cabal cabalInstall digestiveFunctors lens
-    lensAeson safe vector HUnit tasty tastyHunit;
-
-in cabal.mkDerivation (self: {
+{ mkDerivation, aeson, base, bytestring, containers
+, digestive-functors, HUnit, lens, lens-aeson, mtl, safe
+, scientific, stdenv, tasty, tasty-hunit, text, vector
+}:
+mkDerivation {
   pname = "digestive-functors-aeson";
-  version = "1.1.2";
+  version = "1.1.14";
   src = ./.;
-  buildDepends = [ aeson digestiveFunctors lens lensAeson safe vector ];
-  buildTools = [ cabalInstall ];
-  testDepends = [ HUnit tasty tastyHunit ];
-})
+  buildDepends = [
+    aeson base containers digestive-functors lens lens-aeson safe text
+    vector
+  ];
+  testDepends = [
+    aeson base bytestring digestive-functors HUnit mtl scientific tasty
+    tasty-hunit text
+  ];
+  homepage = "http://github.com/ocharles/digestive-functors-aeson";
+  description = "Run digestive-functors forms against JSON";
+  license = stdenv.lib.licenses.gpl3;
+}
