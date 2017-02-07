ca3509d9372d61cebab765b12332baf3bad8a25e
Ollie Charles <ollie@ocharles.org.uk>
2017-02-07 12:52:47 +0000

    Refresh shell.nix

--- shell.nix
+++ shell.nix
@@ -10,7 +10,7 @@ let
       }:
       mkDerivation {
         pname = "digestive-functors-aeson";
-        version = "1.1.18";
+        version = "1.1.21";
         src = ./.;
         libraryHaskellDepends = [
           aeson base containers digestive-functors lens lens-aeson safe text
@@ -25,71 +25,11 @@ let
         license = stdenv.lib.licenses.gpl3;
       };
 
-  overrides = self: super: {
-    lens-aeson = pkgs.haskell.lib.doJailbreak super.lens-aeson;
-    aeson =
-      self.callPackage
-        ({ mkDerivation, attoparsec, base, bytestring, containers, deepseq
-         , dlist, fail, ghc-prim, hashable, HUnit, mtl, QuickCheck
-         , quickcheck-instances, scientific, semigroups, stdenv, syb
-         , template-haskell, test-framework, test-framework-hunit
-         , test-framework-quickcheck2, text, time, transformers
-         , unordered-containers, vector
-         }:
-         mkDerivation {
-           pname = "aeson";
-           version = "0.11.0.0";
-           sha256 = "1mdd4klbad1dx5854agiiixfcc269hnmbam31zmfs91qszaljf5d";
-           libraryHaskellDepends = [
-             attoparsec base bytestring containers deepseq dlist fail ghc-prim
-             hashable mtl scientific semigroups syb template-haskell text time
-             transformers unordered-containers vector
-           ];
-           testHaskellDepends = [
-             attoparsec base bytestring containers ghc-prim HUnit QuickCheck
-             quickcheck-instances template-haskell test-framework
-             test-framework-hunit test-framework-quickcheck2 text time
-             unordered-containers vector
-           ];
-           homepage = "https://github.com/bos/aeson";
-           description = "Fast JSON parsing and encoding";
-           license = stdenv.lib.licenses.bsd3;
-         }) {};
-  };
-
   haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages.override { inherit overrides; }
+                       then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage f {
-          aeson = haskellPackages.callPackage (
-                  { mkDerivation, attoparsec, base, bytestring, containers, deepseq
-                  , dlist, fail, ghc-prim, hashable, HUnit, mtl, QuickCheck
-                  , quickcheck-instances, scientific, semigroups, stdenv, syb
-                  , template-haskell, test-framework, test-framework-hunit
-                  , test-framework-quickcheck2, text, time, transformers
-                  , unordered-containers, vector
-                  }:
-                  mkDerivation {
-                    pname = "aeson";
-                    version = "0.11.0.0";
-                    sha256 = "1mdd4klbad1dx5854agiiixfcc269hnmbam31zmfs91qszaljf5d";
-                    libraryHaskellDepends = [
-                      attoparsec base bytestring containers deepseq dlist fail ghc-prim
-                      hashable mtl scientific semigroups syb template-haskell text time
-                      transformers unordered-containers vector
-                    ];
-                    testHaskellDepends = [
-                      attoparsec base bytestring containers ghc-prim HUnit QuickCheck
-                      quickcheck-instances template-haskell test-framework
-                      test-framework-hunit test-framework-quickcheck2 text time
-                      unordered-containers vector
-                    ];
-                    homepage = "https://github.com/bos/aeson";
-                    description = "Fast JSON parsing and encoding";
-                    license = stdenv.lib.licenses.bsd3;
-                  }) {}; 
-        };
+  drv = haskellPackages.callPackage f {};
 
 in
 
